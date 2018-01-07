4e031e255a4c4ef1339f991ef52a3eb27ddb186a
Marcin Jałbrzykowski <marcin.jalbrzykowski@outlook.com>
2018-01-07 19:55:08 +0100

    Added invoice controller

---
+++ ApiGateway/Controllers/InvoiceController.cs
@@ -0,0 +1,29 @@
+﻿using MassTransit;
+using Messages;
+using Messages.DTO;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiGateway.Controllers
+{
+    [Route("api/invoices")]
+    public class InvoiceController : Controller
+    {
+        private readonly IBus _bus;
+
+        public InvoiceController(IBus bus)
+        {
+            _bus = bus;
+        }
+
+        [HttpPost]
+        public IActionResult CreateInvoice(Invoice invoice)
+        {
+            _bus.Publish(new CreateInvoice
+            {
+                Invoice = invoice
+            });
+
+            return Ok();
+        }
+    }
+}
